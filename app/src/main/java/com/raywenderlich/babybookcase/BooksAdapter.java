d65f596d2294fee833dd2455b4a641d28888c469
shanhong cheng <shnahongcheng@gmail.com>
2016-11-27 11:48:59 -0700

    initial commit

--- app/src/main/java/com/raywenderlich/babybookcase/BooksAdapter.java
+++ app/src/main/java/com/raywenderlich/babybookcase/BooksAdapter.java
@@ -1,26 +1,8 @@
-/*
- * Copyright (c) 2016 Razeware LLC
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 package com.raywenderlich.babybookcase;
+
+/**
+ * Created by scheng on 11/27/16.
+ */
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -34,86 +16,77 @@ import com.squareup.picasso.Picasso;
 
 public class BooksAdapter extends BaseAdapter {
 
-  private final Context mContext;
-  private final Book[] books;
+    private final Context mContext;
+    private final Book[] books;
 
-  public BooksAdapter(Context context, Book[] books) {
-    this.mContext = context;
-    this.books = books;
-  }
-
-  @Override
-  public int getCount() {
-    return books.length;
-  }
-
-  @Override
-  public long getItemId(int position) {
-    return 0;
-  }
-
-  @Override
-  public Object getItem(int position) {
-    return null;
-  }
-
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    final Book book = books[position];
-
-    // standard implementation (should start with this)
-//    if (convertView == null) {
-//      final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-//      convertView = layoutInflater.inflate(R.layout.linearlayout_book, null);
-//    }
-//
-//    final ImageView imageView = (ImageView)convertView.findViewById(R.id.imageview_cover_art);
-//    final TextView nameTextView = (TextView)convertView.findViewById(R.id.textview_book_name);
-//    final TextView authorTextView = (TextView)convertView.findViewById(R.id.textview_book_author);
-//    final ImageView imageViewFavorite = (ImageView)convertView.findViewById(R.id.imageview_favorite);
-//
-//    imageView.setImageResource(book.getImageResource());
-//    nameTextView.setText(mContext.getString(book.getName()));
-//    authorTextView.setText(mContext.getString(book.getAuthor()));
-//    imageViewFavorite.setImageResource(book.getIsFavorite() ? R.drawable.star_enabled : R.drawable.star_disabled);
-
-    // view holder pattern
-    if (convertView == null) {
-      final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-      convertView = layoutInflater.inflate(R.layout.linearlayout_book, null);
-
-      final ImageView imageViewCoverArt = (ImageView)convertView.findViewById(R.id.imageview_cover_art);
-      final TextView nameTextView = (TextView)convertView.findViewById(R.id.textview_book_name);
-      final TextView authorTextView = (TextView)convertView.findViewById(R.id.textview_book_author);
-      final ImageView imageViewFavorite = (ImageView)convertView.findViewById(R.id.imageview_favorite);
-
-      final ViewHolder viewHolder = new ViewHolder(nameTextView, authorTextView, imageViewCoverArt, imageViewFavorite);
-      convertView.setTag(viewHolder);
+    // 1
+    public BooksAdapter(Context context, Book[] books) {
+        this.mContext = context;
+        this.books = books;
     }
 
-    final ViewHolder viewHolder = (ViewHolder)convertView.getTag();
-//    viewHolder.imageViewCoverArt.setImageResource(book.getImageResource());
-    viewHolder.nameTextView.setText(mContext.getString(book.getName()));
-    viewHolder.authorTextView.setText(mContext.getString(book.getAuthor()));
-    viewHolder.imageViewFavorite.setImageResource(book.getIsFavorite() ? R.drawable.star_enabled : R.drawable.star_disabled);
-
-    Picasso.with(mContext).load(book.getImageUrl()).into(viewHolder.imageViewCoverArt);
-
-    return convertView;
-  }
-
-  private class ViewHolder {
-    private final TextView nameTextView;
-    private final TextView authorTextView;
-    private final ImageView imageViewCoverArt;
-    private final ImageView imageViewFavorite;
-
-    public ViewHolder(TextView nameTextView, TextView authorTextView, ImageView imageViewCoverArt, ImageView imageViewFavorite) {
-      this.nameTextView = nameTextView;
-      this.authorTextView = authorTextView;
-      this.imageViewCoverArt = imageViewCoverArt;
-      this.imageViewFavorite = imageViewFavorite;
+    // 2
+    @Override
+    public int getCount() {
+        return books.length;
     }
-  }
+
+    // 3
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // 4
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    // 5
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Book book = books[position];
+
+        // view holder pattern
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.linearlayout_book, null);
+
+            final ImageView imageViewCoverArt = (ImageView)convertView.findViewById(R.id.imageview_cover_art);
+            final TextView nameTextView = (TextView)convertView.findViewById(R.id.textview_book_name);
+            final TextView authorTextView = (TextView)convertView.findViewById(R.id.textview_book_author);
+            final ImageView imageViewFavorite = (ImageView)convertView.findViewById(R.id.imageview_favorite);
+
+            final ViewHolder viewHolder = new ViewHolder(nameTextView, authorTextView, imageViewCoverArt, imageViewFavorite);
+            convertView.setTag(viewHolder);
+        }
+
+        final ViewHolder viewHolder = (ViewHolder)convertView.getTag();
+        // viewHolder.imageViewCoverArt.setImageResource(book.getImageResource());
+
+        Picasso.with(mContext).load(book.getImageResource()).into(viewHolder.imageViewCoverArt);
+
+        viewHolder.nameTextView.setText(mContext.getString(book.getName()));
+        viewHolder.authorTextView.setText(mContext.getString(book.getAuthor()));
+        viewHolder.imageViewFavorite.setImageResource(book.getIsFavorite() ? R.drawable.star_enabled : R.drawable.star_disabled);
+
+        return convertView;
+    }
+
+    // Your "view holder" that holds references to each subview
+    private class ViewHolder {
+        private final TextView nameTextView;
+        private final TextView authorTextView;
+        private final ImageView imageViewCoverArt;
+        private final ImageView imageViewFavorite;
+
+        public ViewHolder(TextView nameTextView, TextView authorTextView, ImageView imageViewCoverArt, ImageView imageViewFavorite) {
+            this.nameTextView = nameTextView;
+            this.authorTextView = authorTextView;
+            this.imageViewCoverArt = imageViewCoverArt;
+            this.imageViewFavorite = imageViewFavorite;
+        }
+    }
 
 }
--- app/src/main/java/com/raywenderlich/babybookcase/MainActivity.java
+++ app/src/main/java/com/raywenderlich/babybookcase/MainActivity.java
@@ -23,17 +23,17 @@
 package com.raywenderlich.babybookcase;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
   private static final String favoritedBookNamesKey = "favoritedBookNamesKey";
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -46,37 +46,43 @@ public class MainActivity extends ActionBarActivity {
 
     gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
       @Override
-      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+      public void onItemClick(AdapterView parent, View view, int position, long id) {
         Book book = books[position];
         book.toggleFavorite();
+
+        // This tells the GridView to redraw itself
+        // in turn calling your BooksAdapter's getView method again for each cell
         booksAdapter.notifyDataSetChanged();
       }
     });
   }
 
+
   @Override
   protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
 
-    final List<Integer> favoritedBookNames = new ArrayList<>();
+    // construct a list of books you've favorited
+    final ArrayList<Integer> favoritedBookNames = new ArrayList<>();
     for (Book book : books) {
       if (book.getIsFavorite()) {
         favoritedBookNames.add(book.getName());
       }
     }
 
-    outState.putIntegerArrayList(favoritedBookNamesKey, (ArrayList)favoritedBookNames);
+    // save that list to outState for later
+    outState.putIntegerArrayList(favoritedBookNamesKey, favoritedBookNames);
   }
 
   @Override
   protected void onRestoreInstanceState(Bundle savedInstanceState) {
     super.onRestoreInstanceState(savedInstanceState);
 
-    final List<Integer> favoritedBookNames =
-      savedInstanceState.getIntegerArrayList(favoritedBookNamesKey);
+    // get our previously saved list of favorited books
+    final ArrayList<Integer> favoritedBookNames =
+            savedInstanceState.getIntegerArrayList(favoritedBookNamesKey);
 
-    // warning: typically you should avoid n^2 loops like this, use a Map instead.
-    // I'm keeping this because it is more straightforward
+    // look at all of your books and figure out which are the favorites
     for (int bookName : favoritedBookNames) {
       for (Book book : books) {
         if (book.getName() == bookName) {
@@ -89,25 +95,25 @@ public class MainActivity extends ActionBarActivity {
 
   private Book[] books = {
     new Book(R.string.abc_an_amazing_alphabet_book, R.string.dr_seuss, R.drawable.abc,
-      "http://ecx.images-amazon.com/images/I/51rFg8xKvxL._AA240_FMwebp_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/abc.jpg"),
     new Book(R.string.are_you_my_mother, R.string.dr_seuss, R.drawable.areyoumymother,
-      "http://ecx.images-amazon.com/images/I/51Zu5zbzWDL._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/areyoumymother.jpg"),
     new Book(R.string.where_is_babys_belly_button, R.string.karen_katz, R.drawable.whereisbabysbellybutton,
-      "http://ecx.images-amazon.com/images/I/51scITk38DL._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/whereisbabysbellybutton.jpg"),
     new Book(R.string.on_the_night_you_were_born, R.string.nancy_tillman, R.drawable.onthenightyouwereborn,
-      "http://ecx.images-amazon.com/images/I/51pny-HGhnL._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/onthenightyouwereborn.jpg"),
     new Book(R.string.hand_hand_fingers_thumb, R.string.dr_seuss, R.drawable.handhandfingersthumb,
-      "http://ecx.images-amazon.com/images/I/51W+iGZ-dkL._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/handhandfingersthumb.jpg"),
     new Book(R.string.the_very_hungry_caterpillar, R.string.eric_carle, R.drawable.theveryhungrycaterpillar,
-      "http://ecx.images-amazon.com/images/I/41zqrOnjpTL._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/theveryhungrycaterpillar.jpg"),
     new Book(R.string.the_going_to_bed_book, R.string.sandra_boynton, R.drawable.thegoingtobedbook,
-      "http://ecx.images-amazon.com/images/I/61TU+6P-3yL._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/thegoingtobedbook.jpg"),
     new Book(R.string.oh_baby_go_baby, R.string.dr_seuss, R.drawable.ohbabygobaby,
-      "http://ecx.images-amazon.com/images/I/518wNY-AF7L._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/ohbabygobaby.jpg"),
     new Book(R.string.the_tooth_book, R.string.dr_seuss, R.drawable.thetoothbook,
-      "http://ecx.images-amazon.com/images/I/51RFYYCYRTL._AA240_QL65_.jpg"),
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/thetoothbook.jpg"),
     new Book(R.string.one_fish_two_fish_red_fish_blue_fish, R.string.dr_seuss, R.drawable.onefish,
-      "http://ecx.images-amazon.com/images/I/51dYLc1uKsL._AA240_QL65_.jpg")
+      "http://www.raywenderlich.com/wp-content/uploads/2016/03/onefish.jpg")
   };
 
 }
